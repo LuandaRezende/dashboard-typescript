6ddf0eb2d7267332b4ba71ec9eda8f41e44d439e
LuandaRezende <luanda.rezende@hotmail.com>
2022-10-03 12:56:16 -0300

    feat: add dark and light theme

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Button } from '@mui/material';
+import { useAppThemeContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
+    const  { toggleTheme } = useAppThemeContext();
     return (
         <Routes>
-            <Route path='/home' element={<Button variant='contained' color='primary'>teste</Button>} />
+            <Route path='/home' element={<Button variant='contained' color='primary' onClick={toggleTheme}>teste</Button>} />
             <Route path='*' element={<Navigate to='/home'></Navigate>} />
         </Routes>
     );
