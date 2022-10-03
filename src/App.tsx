ad609ec7ccc1c5798ff947c43649d970c9a59190
LuandaRezende <luanda.rezende@hotmail.com>
2022-10-03 11:16:28 -0300

    feat: folder structuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
 export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Luandinha zik4
-        </a>
-      </header>
+      <p>teste</p>
     </div>
   );
   }
