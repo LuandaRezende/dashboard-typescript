922578385733956731b68da620b41a6d74a0108a
LuandaRezende <luanda.rezende@hotmail.com>
2022-11-05 14:35:16 -0300

    feat: create component text field

--- src/pages/people/PeopleDetails.tsx
+++ src/pages/people/PeopleDetails.tsx
@@ -1,9 +1,84 @@
-import { useParams } from 'react-router-dom';
+import { LinearProgress } from '@mui/material';
+import { Form } from '@unform/web';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { ToobarDetail } from '../../shared/components';
+import { VTextField } from '../../shared/forms';
+import { BaseLayout } from '../../shared/layouts';
+import { PeopleService } from '../../shared/services/api/people/PeopleService';
 
 export const PeopleDetails: React.FC = () => {
-  const { id = 'nova' } = useParams<'id'>();
+  const { id = 'new' } = useParams<'id'>();
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    if(id !== 'new'){
+      setIsLoading(true);
+      PeopleService.getById(Number(id)).then(
+        (result) => {
+          setIsLoading(false);  
+
+          if(result instanceof Error){
+            alert(result.message);
+            navigate('/people');
+          }else{
+            setName(result.fullName);
+            console.log('r', result);
+          } 
+        }
+      ); 
+    }
+  }, [id]);
+
+  const handleSave = () => {
+    console.log('save');
+  };
+
+  const handleDelete = (id: number) => {
+    if(confirm('Realmente deseja apagar?')){
+      PeopleService.deleteById(id)
+        .then(result => {
+          if(result instanceof Error){
+            alert(result.message);
+          }else{
+            alert('Registro apagado com sucesso!');
+            navigate('/people');
+          }
+        });
+    }
+  };
     
   return (
-    <p>detalhe de pessoas  {id}</p>
+    <BaseLayout 
+      title={id === 'new' ? 'Nova pessoa' : `Edição de ${name}`}
+      toolbar={
+        <ToobarDetail 
+          textNewButton="Nova"
+          showSaveCloseButton
+          showNewButton={id !== 'new'}
+          showDeleteButton={id !== 'new'}
+
+          saveItem={() => {handleSave();}}
+          saveAndCloseItem={() => {handleSave();}}
+          deleteItem={() => handleDelete(Number(id))}
+          newItem={() => {navigate('/people/details/new');}}
+          returnItem={() => {navigate('/people');}}
+        />
+      }
+    >
+
+      <Form onSubmit={(dataForm) => console.log(dataForm)} initialData={{}}>
+        <VTextField name="fullName" />
+      </Form>
+
+      {isLoading && (
+        <LinearProgress variant="indeterminate"></LinearProgress>
+      )}
+
+      <p>detalhe de pessoas  {id}</p>
+
+    </BaseLayout>
   );
 };
